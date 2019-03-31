fe9e655126658ad84d153356bcf1b14d59580b1f
Alon <222alon123@gmail.com>
2019-03-31 19:00:23 +0300

    alon and dan, added implementation of "Interests", created activity "InterestsHandlerActivity" + layout

--- app/src/main/java/com/kalay/shift/shift/Classes/Interests.java
+++ app/src/main/java/com/kalay/shift/shift/Classes/Interests.java
@@ -2,22 +2,33 @@ package com.kalay.shift.shift.Classes;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Interests {
-    private List<String> interests = new ArrayList<>();
-    private Map<String, List<Notification> > notifications = new HashMap<>();
-    public List<String> getInterests(){
+    //private List<String> interests = new ArrayList<>();
+    private Map<String, List<Notification>> notifications = new LinkedHashMap<>();
 
+    public List<String> getInterests() {
+        return new ArrayList<>(notifications.keySet());
     }
-    public List<Notification> getNotification (String interest){
 
+    public List<Notification> getNotification(String interest) {
+        return notifications.get(interest);
     }
-    public void addInterest(String Interest){
 
+    public void addInterest(String interest) {
+        if (!notifications.containsKey(interest))
+            notifications.put(interest, new ArrayList<Notification>());
     }
-    public void addNotification(String interest, Notification notification){
 
+    public void addNotification(String interest, Notification notification) {
+        List<Notification> value = notifications.get(interest);
+        if (value == null) {
+            value = new ArrayList<Notification>();
+            notifications.put(interest, value);
+        }
+        value.add(notification);
     }
 }
